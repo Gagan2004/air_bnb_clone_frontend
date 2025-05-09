cf513f00b72480e7074871870bb3a7dcd9434a7b
Gagan2004 <gagank90815@gmail.com>
2025-05-09 10:05:45 +0530

    server and dashboard

--- src/api/axiosInstance.jsx
+++ src/api/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
   
   const axiosInstance = axios.create({
-    baseURL: "https://your-backend.onrender.com/api",
+    baseURL: "https://air-bnb-clone-backend.onrender.com/api",
     headers: {
       'Content-Type': 'application/json',
     },
